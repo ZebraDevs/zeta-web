87d405c252b3a5e128ef2d0c54855fa72834eca4
SvilenIvanov <svilen.ivanov@athlonsofia.com>
2023-12-04 12:36:06 +0200

    temporary update

--- src/components/badges/text-badge/text-badge.stories.ts
+++ src/components/badges/text-badge/text-badge.stories.ts
@@ -1,14 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { ZetaTextBadge } from "./text-badge.js";
-import "./priority-pill.js";
+import "./text-badge.js";
+
 const meta: Meta<ZetaTextBadge> = {
-  component: "zeta-priority-pill",
+  component: "zeta-text-badge",
   args: {
     text: "label",
     rounded: true
   },
-  argTypes: {}
+  argTypes: {
+    status: {
+      options: ["neutral", "info", "positive", "warning", "negative"],
+      control: {
+        type: "select"
+      }
+    }
+  }
 };
+
 export default meta;
 
 export const TextBadge: StoryObj<ZetaTextBadge> = {};
--- src/components/badges/text-badge/text-badge.ts
+++ src/components/badges/text-badge/text-badge.ts
@@ -1,18 +1,17 @@
-import { html, svg } from "lit";
+import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { BadgeStatus } from "../../../types.js";
 import { ContourableCondensableElement } from "../../../mixins/condense.js";
 
-/** ZetaStatusLabel web component.
+/** ZetaTextLabel web component.
  *
  * To help some information, labels, or errors stand out, we present them with badges. They can look like buttons, but users can’t select them. They just guide users to things they should pay attention to.
  *
- * Slotted children:
+ * children:
  *    * Text
- *    * Icon
  *
  * @public */
-@customElement("zeta-status-label")
+@customElement("zeta-text-badge")
 export class ZetaTextBadge extends ContourableCondensableElement {
   /** Type of text label.
    *
@@ -25,6 +24,8 @@ export class ZetaTextBadge extends ContourableCondensableElement {
   @property({ type: String }) text: string | undefined;
 
   protected override render() {
+    console.log(this.status);
+
     return html`
       <div class="container">
         <div class="text">${this.text && this.text}</div>
