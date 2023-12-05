d95f3594af5b912485eb723d0beec37573350594
SvilenIvanov <svilen.ivanov@athlonsofia.com>
2023-12-05 13:36:25 +0200

    created the header sheet component

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ import "../src/components/tag/tag.ts";
 import "../src/components/badges/text-badge/text-badge.ts";
 import "../src/components/accordion/accordion.ts";
 import "../src/components/workcloud-indicator/workcloud-indicator.ts";
+import "../src/components/bottom-sheets/sheet-header/sheet-header.ts";
 export * from "../src/components/badges/priority-pill.js";
 export * from "../src/components/badges/status-label.js";
 export * from "../src/components/banner/in-page-banner.js";
@@ -30,4 +31,5 @@ export * from "../src/components/tag/tag.js";
 export * from "../src/components/accordion/accordion.js";
 export * from "../src/components/badges/text-badge/text-badge.js";
 export * from "../src/components/workcloud-indicator/workcloud-indicator.js";
+export * from "../src/components/bottom-sheets/sheet-header/sheet-header.js";
 
