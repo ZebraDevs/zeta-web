e8a2a0a584fa94efd207e5b507a2694fbaf59635
mikecoomber <58986130+mikecoomber@users.noreply.github.com>
2024-01-18 14:53:59 +0000

    chore: Generating test files and adding to index in the create web component script (#60)

--- scripts/makeWebComponentTemplate.js
+++ scripts/makeWebComponentTemplate.js
@@ -1,43 +1,69 @@
-import { write, log, replaceAllInTemplate, capitalize } from './utils.js';
-import { readFileSync, existsSync } from 'fs';
-
+import { write, log, replaceAllInTemplate, capitalize, insert } from "./utils.js";
+import { readFileSync, existsSync, writeFileSync } from "fs";
+import path from "path";
 
 const buildFile = (nameLower, nameCap, outDir, template, type) => {
-    if (existsSync(path.normalize(outDir))) {
-        log('⚔️ --- ' + nameCap + ' ' + type + ' already exists')
-    } else {
-        let populatedTemplate = template;
-        populatedTemplate = replaceAllInTemplate('_replacelower_', nameLower, populatedTemplate)
-        populatedTemplate = replaceAllInTemplate('_replacecap_', nameCap, populatedTemplate)
-        write(outDir, populatedTemplate);
-        log(' --- Zeta' + nameCap + ' ' + type + ' created\n')
-    }
-}
+  if (existsSync(path.normalize(outDir))) {
+    log("⚔️ --- " + nameCap + " " + type + " already exists");
+  } else {
+    let populatedTemplate = template;
+    populatedTemplate = replaceAllInTemplate("_replacelower_", nameLower, populatedTemplate);
+    populatedTemplate = replaceAllInTemplate("_replacecap_", nameCap, populatedTemplate);
+    write(outDir, populatedTemplate);
+    log(" --- Zeta" + nameCap + " " + type + " created\n");
+  }
+};
+
+const appendIndex = outDir => {
+  const indexPath = "../src/index.ts";
+  const indexFile = readFileSync(indexPath);
+  if (indexFile) {
+    let indexFileStr = String(indexFile);
+
+    const importStatement = 'import "' + outDir + '.ts";' + "\n";
+    const importStatementLocation = indexFileStr.indexOf("export") - 2;
+
+    indexFileStr = insert(indexFileStr, importStatement, importStatementLocation);
+
+    const exportStatement = 'export * from "' + outDir + '.js";';
+
+    indexFileStr += exportStatement;
+
+    log(indexFileStr);
+    writeFileSync(indexPath, indexFileStr);
+  }
+};
 
 const main = () => {
+  const nameArr = process.argv.slice(2).filter(x => x);
+  let nameLower,
+    dirName = null;
+  nameLower = nameArr[0].toLowerCase();
+  if (nameArr.length == 1) {
+    // dir and file same name
+    dirName = nameLower;
+  } else if (nameArr.length == 2) {
+    // dir and file differnet names
+    dirName = nameArr[1];
+  } else {
+    log("❎ --- No name entered");
+  }
 
-    const nameArr = process.argv.slice(2).filter(x => x)
-    let nameLower, dirName = null;
-    nameLower = nameArr[0].toLowerCase();
-    if (nameArr.length == 1) {
-        // dir and file same name
-        dirName = nameLower;
-    } else if (nameArr.length == 2) {
-        // dir and file differnet names
-        dirName = nameArr[1];
-    } else {
-        log('❎ --- No name entered')
-    }
+  if (dirName) {
+    const outDir = "../src/components/" + dirName + "/" + nameLower;
+    const testOutDir = "../test/" + dirName + "/" + nameLower;
+    const nameCap = nameLower
+      .split("-")
+      .map(x => capitalize(x))
+      .join("");
 
-    if (dirName) {
-        const outDir = '../src/components/' + dirName + '/' + nameLower
-        const nameCap = nameLower.split('-').map(x => capitalize(x))
+    buildFile(nameLower, nameCap, outDir + ".ts", readFileSync(path.normalize("./assets/web.template")), "Component");
+    buildFile(nameLower, nameCap, outDir + ".scss", readFileSync(path.normalize("./assets/web.styles.template")), "Style");
+    buildFile(nameLower, nameCap, outDir + ".stories.ts", readFileSync(path.normalize("./assets/web.stories.template")), "Story");
+    buildFile(nameLower, nameCap, testOutDir + ".test.ts", readFileSync(path.normalize("./assets/web.test.template")), "Test");
 
-        buildFile(nameLower, nameCap, outDir + '.ts', readFileSync(path.normalize('./assets/web.template')), 'Component')
-        buildFile(nameLower, nameCap, outDir + '.styles.ts', readFileSync(path.normalize('./assets/web.styles.template')), 'Style')
-        buildFile(nameLower, nameCap, outDir + '.stories.ts', readFileSync(path.normalize('./assets/web.stories.template')), 'Story')
-        //TODO: Add to index
-    }
-}
+    appendIndex(outDir);
+  }
+};
 
 main();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,42 +1,56 @@
-import { mkdirSync, writeFileSync } from 'fs'
-import * as path from 'path';
+import { mkdirSync, writeFileSync } from "fs";
+import * as path from "path";
 
 /** Custom wrapper for `console.log` to account for --silent arg.
- * 
+ *
  * @param {string} string string to be logged.
  */
-export const log = (string) => {
-    if (!process.argv.slice(2).includes('--silent')) {
-        console.log(string)
-    }
-}
+export const log = string => {
+  if (!process.argv.slice(2).includes("--silent")) {
+    console.log(string);
+  }
+};
 
 /** Custom wrapper for `writeFileSync` to account for --dry-run arg.
- * 
+ *
  * @param {string} pathName Path of file to be written.
  * @param {String} contents Contents to be written to file.
  */
 export const write = (pathName, contents) => {
-    if (!process.argv.slice(2).includes('--dry-run')) {
-        const dir = pathName.split('/')
-        dir.pop();
-        mkdirSync(path.normalize(dir.join('/')), { recursive: true })
-        writeFileSync(path.normalize(pathName), contents);
-    }
-}
+  if (!process.argv.slice(2).includes("--dry-run")) {
+    const dir = pathName.split("/");
+    dir.pop();
+    mkdirSync(path.normalize(dir.join("/")), { recursive: true });
+    writeFileSync(path.normalize(pathName), contents);
+  }
+};
 
 /** Finds and replaces all instances of a search with replacement in template.
- * 
+ *
  * @param {string} search String to be replaced.
  * @param {string} replacement Replacement string.
  * @param {string} template String to be searched.
  * @returns {string} String with replacements made.
  */
-export const replaceAllInTemplate = (search, replacement, template) => String(template).replace(new RegExp(search, 'g'), replacement);
+export const replaceAllInTemplate = (search, replacement, template) => String(template).replace(new RegExp(search, "g"), replacement);
 
 /** Capitalizes all words in string.
- * 
+ *
  * @param {string} s string to be parsed.
  * @returns {string} All words in string capitalized.
-*/
-export const capitalize = s => s.split('-').map(w => w.charAt(0).toUpperCase() + w.slice(1)).join('')
+ */
+export const capitalize = s =>
+  s
+    .split("-")
+    .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+    .join("");
+
+/**
+ * Inserts a string into another string at a given location.
+ *
+ * @param {string} string The string to be added to.
+ * @param {string} newString The string to be inserted.
+ * @param {int} index The index for the new string to be insterted at.
+ * @returns The new string;
+ */
+export const insert = (string, newString, index) => string.slice(0, index) + newString + string.slice(index, string.length);
