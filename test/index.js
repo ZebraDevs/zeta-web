7fae32f8f76574d0cf41203222a330b619f716ae
thelukewalton <lwalton@zebra.com>
2025-02-25 10:27:02 +0000

    chore: Remove unused test directory

--- test/index.js
+++
@@ -1,8 +0,0 @@
-// import "@zebra-fed/zeta-web";
-import "@zebra-fed/zeta-web/components/button/button";
-import "@zebra-fed/zeta-web/index.css";
-
-document.querySelector("#root").innerHTML =
-  "<zeta-button>Click me</zeta-button>";
-
-console.error("WHY IS THIS NOT WORKING?");
