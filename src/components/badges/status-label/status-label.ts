fe31aa67b3c2c1ea0bf80d05bd1f800f68d4dc43
thelukewalton <lwalton@zebra.com>
2025-06-23 20:00:53 +0100

    status label

--- src/components/badges/status-label/status-label.ts
+++ src/components/badges/status-label/status-label.ts
@@ -1,16 +1,13 @@
-import { html, LitElement, svg } from "lit";
+import { html, LitElement, nothing, svg } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import styles from "./status-label.styles.js";
 import { type ZetaIconName } from "@zebra-fed/zeta-icons";
 import { Contourable } from "../../../mixins/mixins.js";
 import "../../icon/icon.js";
-import { styleMap } from "lit/directives/style-map.js";
 /**
  * To help some information, labels, or errors stand out, we present them with badges. They can look like buttons, but users can’t select them. They just guide users to things they should pay attention to.
  *
- * Slotted children:
- *    * Text
- *    * Icon
+ * @slot - Text displayed on label.
  *
  * @figma https://www.figma.com/file/JesXQFLaPJLc1BdBM4sisI/%F0%9F%A6%93-ZDS---Components?type=design&node-id=21836-37274
  * @storybook https://design.zebra.com/web/storybook/?path=/docs/components-badges--docs
@@ -22,7 +19,12 @@ export class ZetaStatusLabel extends Contourable(LitElement) {
 
   /** Text displayed on label.
    *
-   * Can also be slotted. */
+   * Can also be slotted.
+   *
+   * @defaultValue `undefined`. This will render a slot for text.
+   *
+   * If provided, it will override the slot.
+   * */
   @property({ type: String }) text?: string;
 
   /** Icon leading the component. @see {ZetaIconName}.
@@ -30,25 +32,27 @@ export class ZetaStatusLabel extends Contourable(LitElement) {
    * @defaultValue `undefined`. This will render an indicator circle.
    */
   @property({ type: String }) icon?: ZetaIconName;
+
+  /** Whether to show a custom icon.
+   *
+   * An icon must be provided for this to have an effect.
+   */
+  @property({ type: Boolean }) showIcon = true;
+
   static styles = [super.styles ?? [], styles];
 
   protected override render() {
     const icon = this.icon
-      ? html`<zeta-icon
-          .rounded=${this.rounded}
-          style=${styleMap({
-            "--icon-color": `var(--icon-" + ${this.status} + ")`
-          })}
-          >${this.icon}</zeta-icon
-        >`
+      ? html`<zeta-icon .rounded=${this.rounded}>${this.icon}</zeta-icon>`
       : svg`
     <svg xmlns="http://www.w3.org/2000/svg" width="8" height="20" viewBox="0 0 8 8" >
     <circle cx="4" cy="4" r="4" />
     </svg>`;
+
     return html`
       <div class="container">
-        <div class="icon-container">${icon}</div>
-        <div class="text">${this.text ? this.text : html`<slot class="text" name="text"></slot>`}</div>
+        ${(this.icon && this.showIcon) || !this.icon ? html`<div class="icon-container">${icon}</div>` : nothing}
+        <div class="text">${this.text ? this.text : html`<slot></slot>`}</div>
       </div>
     `;
   }
