c538ba6869b6d724b475596fc22515e6db8751f9
thelukewalton <lwalton@zebra.com>
2025-06-23 20:02:42 +0100

    priority pill

--- src/components/badges/badges.ts
+++ src/components/badges/badges.ts
@@ -3,4 +3,3 @@ export * from "./priority-pill/priority-pill.js";
 export * from "./status-label/status-label.js";
 export * from "./tag/tag.js";
 export * from "./label/label.js";
-export * from "./workcloud-indicator/workcloud-indicator.js";
--- src/stories/components/Badges/priority-pill.stories.ts
+++ src/stories/components/Badges/priority-pill.stories.ts
@@ -1,23 +1,57 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { ZetaPriorityPill } from "../../../components/badges/priority-pill/priority-pill.js";
+import { spreadGenerator } from "../../utils.js";
+import { html } from "lit";
+
+const spread = spreadGenerator(ZetaPriorityPill);
 
 const meta: Meta<ZetaPriorityPill> = {
   title: "Components/Badges",
   component: "zeta-priority-pill",
   args: {
-    text: "Priority",
-    number: 1,
-    rounded: true
+    rounded: true,
+    status: "urgent",
+    type: "lozenge",
+    size: "large"
+  },
+  argTypes: {
+    status: {
+      options: ["urgent", "high", "medium", "low"],
+      control: { type: "inline-radio" }
+    },
+    size: {
+      options: ["small", "large"],
+      control: { type: "inline-radio" }
+    },
+    type: {
+      options: ["badge", "lozenge"],
+      control: { type: "inline-radio" }
+    },
+    slot: { table: { disable: true } }
   },
   parameters: {
     design: {
       url: "https://www.figma.com/file/JesXQFLaPJLc1BdBM4sisI/%F0%9F%A6%93-ZDS---Components?type=design&node-id=21931-2105&mode=design&t=j9Cv98TDx5BKLbgS-4"
     },
     status: {
-      type: "needsAttention"
+      type: "ready"
     }
   }
 };
 export default meta;
 
-export const PriorityPill: StoryObj<ZetaPriorityPill> = {};
+export const PriorityPill: StoryObj = {
+  render: args => {
+    return html`
+      <style>
+        zeta-priority-pill {
+          ${args["--priority-pill-index-background-color"] && html`--priority-pill-index-background-color: ${args["--priority-pill-index-background-color"]}`};
+          ${args["--priority-pill-index-text-color"] && html`--priority-pill-index-text-color: ${args["--priority-pill-index-text-color"]}`};
+          ${args["--priority-pill-text-color"] && html`--priority-pill-text-color: ${args["--priority-pill-text-color"]}`};
+          ${args["--priority-pill-background-color"] && html`--priority-pill-background-color: ${args["--priority-pill-background-color"]}`};
+        }
+      </style>
+      <zeta-priority-pill ${spread(args)}> </zeta-priority-pill>
+    `;
+  }
+};
