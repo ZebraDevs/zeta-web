75be19f4c7e4be0724134f1dacdd5fb1ea5e8eaa
aygurs <aydin_gurs@hotmail.com>
2025-08-04 13:57:21 +0100

    feat(UX-1482): Created tile-button and tests for tile-button
    Added sinon for mocking functions.

---
+++ src/test/button/tile-button.test.ts
@@ -0,0 +1,156 @@
+import { fixture, html, expect, unsafeStatic, elementUpdated } from "@open-wc/testing";
+import type { ZetaTileButton } from "../../components/button/tile-button/tile-button.js";
+import "../../components/button/tile-button/tile-button.js";
+
+import "../../index.css";
+import { getCssVarColorValue, getSlotText, KeyboardActions, MouseActions } from "../utils.js";
+import sinon from "sinon";
+import { contrastTest } from "../accessibility-utils/accessibility-test-runner.js";
+
+describe("zeta-tile-button", () => {
+  let subject: ZetaTileButton;
+
+  const createComponent = (template = `<zeta-tile-button icon="star">Button</zeta-tile-button>`) => {
+    // prettier-ignore
+    return fixture<ZetaTileButton>(html`${unsafeStatic(template)}`);
+  };
+
+  beforeEach(async () => {
+    subject = await createComponent();
+  });
+
+  const createDisabledButton = async (subject: ZetaTileButton) => {
+    subject.remove();
+    await elementUpdated(subject);
+    subject = await createComponent(`<zeta-tile-button icon="star" disabled>Button</zeta-tile-button>`);
+    await elementUpdated(subject);
+  };
+
+  describe("Accessibility", () => {
+    it("meets aria requirements", async () => {
+      await expect(subject).to.be.accessible();
+      await expect(subject).shadowDom.to.be.accessible();
+    });
+
+    it("meets contrast requirements for outline-subtle flavor", async () => {
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      await contrastTest(`Button outline-subtle`, srButton, srButton);
+    });
+  });
+
+  describe("Content", () => {
+    it("renders an icon", async () => {
+      const icon = subject.shadowRoot?.querySelector("zeta-icon");
+      await expect(icon?.name).to.equal("star");
+    });
+
+    it("renders a default icon if none is provided", async () => {
+      subject.icon = undefined;
+      await elementUpdated(subject);
+      const icon = subject.shadowRoot?.querySelector("zeta-icon");
+      await expect(icon?.name).to.equal("star");
+    });
+
+    it("renders a label", async () => {
+      await expect(getSlotText(subject)).to.equal("Button");
+    });
+
+    it("text stays empty when no text is given", async () => {
+      subject.slot = "";
+      await elementUpdated(subject);
+      await expect(subject.slot).to.equal("");
+    });
+  });
+
+  describe("Dimensions", () => {
+    it("if characters in title equal less than 80px, width should == 80px", async () => {
+      subject.textContent = "1234567";
+      await elementUpdated(subject);
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).width).to.equal("80px");
+    });
+
+    it("if characters in title exceed 80px, width will be more than 80px", async () => {
+      subject.textContent = "12345678910111213";
+      await elementUpdated(subject);
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).width).to.not.equal("80px");
+    });
+
+    it("height should == 80px regardless of title.length", async () => {
+      subject.slot = "";
+      await elementUpdated(subject);
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).height).to.equal("80px");
+      subject.slot = "12345678910111213";
+      await elementUpdated(subject);
+      expect(getComputedStyle(srButton).height).to.equal("80px");
+    });
+  });
+
+  describe("Styling", () => {
+    it("has a white background", async () => {
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).backgroundColor).to.equal(getCssVarColorValue(srButton, "--state-default-enabled"));
+    });
+
+    it("has black text", async () => {
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).color).to.equal(getCssVarColorValue(srButton, "--main-default"));
+    });
+
+    it("has a black icon", async () => {
+      const srZetaIcon = subject.shadowRoot?.querySelector("zeta-icon")!;
+      expect(getComputedStyle(srZetaIcon).color).to.equal(getCssVarColorValue(srZetaIcon, "--main-default"));
+    });
+
+    it("has border-default border", async () => {
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      const buttonBorderColor = getComputedStyle(srButton).boxShadow.match(/rgb[a]?\([^)]+\)/)?.[0] ?? "rgb(0,0,0)";
+      expect(buttonBorderColor).to.equal(getCssVarColorValue(srButton, "--border-default"));
+    });
+
+    it("has the correct background colour when disabled", async () => {
+      await createDisabledButton(subject);
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).backgroundColor).to.equal(getCssVarColorValue(srButton, "--surface-disabled"));
+    });
+
+    it("has the correct icon colour when disabled", async () => {
+      await createDisabledButton(subject);
+      const srZetaIcon = subject.shadowRoot?.querySelector("zeta-icon")!;
+      expect(getComputedStyle(srZetaIcon).color).to.equal(getCssVarColorValue(srZetaIcon, "--main-disabled"));
+    });
+
+    it("has the correct text colour when disabled", async () => {
+      await createDisabledButton(subject);
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      expect(getComputedStyle(srButton).color).to.equal(getCssVarColorValue(srButton, "--main-disabled"));
+    });
+  });
+
+  describe("Interaction", () => {
+    it("calls onClick when clicked", async () => {
+      const eventSpy = sinon.spy();
+      subject.addEventListener("click", eventSpy);
+      await MouseActions.click(subject);
+      expect(eventSpy).to.have.been.calledOnce;
+    });
+
+    it("has the correct style when hovered", async () => {
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      await MouseActions.hover(srButton);
+      expect(getComputedStyle(srButton).backgroundColor).to.equal(getCssVarColorValue(srButton, "--surface-hover"));
+    });
+
+    it("has the correct style when focused", async () => {
+      const srButton = subject.shadowRoot?.querySelector("button")!;
+      await KeyboardActions.press("Tab");
+      expect(getComputedStyle(srButton).outlineColor).to.equal(getCssVarColorValue(srButton, "--border-primary"));
+    });
+  });
+
+  // describe("Golden", () => {});
+
+  // describe("Performance", () => {});
+});
