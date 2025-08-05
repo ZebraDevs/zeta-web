9226eac18d7f72cc825efb8d1ff89f2012c21910
aygurs <aydin.gursoy@zebra.com>
2025-08-05 14:49:21 +0100

    feat(UX-1459): Dialog styles match figma designs and functionality

--- src/components/dialog/dialog.styles.js
+++ src/components/dialog/dialog.styles.js
@@ -3,8 +3,17 @@ export default css`
   :host {
     --_dialog-width: var(--dialog-width, 480px);
     --_dialog-max-height: var(--dialog-max-height, 80vh);
-    --_dialog-font-size: var(--dialog-font-size, 1.25rem);
-    --_dialog-line-height: var(--dialog-line-height, 1.5rem);
+    --_dialog-title-font-size: var(--dialog-title-font-size, 1.25rem);
+    --_dialog-title-line-height: var(--dialog-title-line-height, 1.5rem);
+
+    footer[data-element-count="3"] {
+      justify-content: space-between;
+    }
+
+    & .actions,
+    & ::slotted([slot="other"]) {
+      width: fit-content;
+    }
   }
 
   dialog {
@@ -66,19 +75,14 @@ export default css`
     }
   }
 
-  /*::slotted([slot="other"]) {
-    display: flex;
-    width: fit-content;
-  }*/
-
   h1 {
     display: -webkit-box;
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 1;
     flex: 1 0 0;
     font: var(--headline-small);
-    font-size: var(--_dialog-font-size);
-    line-height: var(--_dialog-line-height);
+    font-size: var(--_dialog-title-font-size);
+    line-height: var(--_dialog-title-line-height);
     margin: 0;
     padding: 0;
   }
@@ -89,62 +93,34 @@ export default css`
     flex: 1;
     min-height: 1.125rem;
     overflow: auto;
-  }
-
-  /*default size = small*/
-  :host(:not([size="large"])) div[part="body"] {
-    font: var(--body-small);
-    min-height: 1.125rem;
-  }
-
-  :host([size="large"]) div[part="body"] {
     /*Note: the designs as of 28/11/2024 show a different font size for the large dialog body ("Standard/Body/Large"). This isnt in ZDS tokens and needs to be changed in Figma*/
     font: var(--body-medium);
     min-height: 1.5rem;
   }
 
-  :host(:not([size="large"])) {
-    footer {
-      flex-direction: row;
-    }
-    footer[data-element-count="3"] {
-      flex-direction: column-reverse;
-      & .actions {
-        flex-direction: column-reverse;
-      }
+  :host([flavor="info"]) {
+    & [part="header-icon"] {
+      --icon-color: var(--main-info);
     }
   }
-
-  :host([size="large"]) {
-    footer[data-element-count="3"] {
-      justify-content: space-between;
-    }
-
-    & .actions,
-    & ::slotted([slot="other"]) {
-      width: fit-content;
+  :host([flavor="warning"]) {
+    & [part="header-icon"] {
+      --icon-color: var(--main-warning);
     }
   }
-
+  :host([flavor="success"]) {
+    & [part="header-icon"] {
+      --icon-color: var(--main-positive);
+    }
+  }
+  :host([flavor="error"]) {
+    & [part="header-icon"] {
+      --icon-color: var(--main-negative);
+    }
+  }
   zeta-icon {
     --icon-size: 24px;
   }
-  ::slotted(zeta-icon[name="block"]) {
-    --icon-color: var(--main-default);
-  }
-  ::slotted(zeta-icon[name="info"]) {
-    --icon-color: var(--main-info);
-  }
-  ::slotted(zeta-icon[name="verified"]) {
-    --icon-color: var(--main-positive);
-  }
-  ::slotted(zeta-icon[name="warning"]) {
-    --icon-color: var(--main-warning);
-  }
-  ::slotted(zeta-icon[name="error"]) {
-    --icon-color: var(--main-negative);
-  }
-
   zeta-icon[name="close"] {
     cursor: pointer;
   }
--- src/components/dialog/dialog.ts
+++ src/components/dialog/dialog.ts
@@ -4,11 +4,7 @@ import { LitElement, html } from "lit";
 import styles from "./dialog.styles.js";
 import { ZetaButton } from "../button/button.js";
 import { Contourable, Popup } from "../../mixins/mixins.js";
-import type { ZetaIconName } from "@zebra-fed/zeta-icons";
 
-//Extract only icons compatible with dialog box
-export type DialogIcons = Extract<ZetaIconName, "block" | "info" | "verified" | "warning" | "error">;
-export const DialogIconList: DialogIcons[] = ["block", "info", "verified", "warning", "error"];
 /*
  * TODO: dialog Autofocus.
  */
@@ -26,7 +22,9 @@ export const DialogIconList: DialogIcons[] = ["block", "info", "verified", "warn
  * When shown as a modal, clicking the background barrier will close the modal by default; this can be changed with the `closeOnBarrierClicked` property.
  *
  * @slot - Body of dialog; typically text.
- * @slot {zeta-icon} icon - A `zeta-icon` element. Size will be restricted based on dialog type.
+ * @slot {zeta-icon} icon - A `zeta-icon` element. Size will be restricted based on dialog type. Icon type and colour will be
+ * restricted based on dialog flavour.
+ * @slot {zeta-icon} icon - A `zeta-icon` element. The close icon in the header.
  * @slot {zeta-button} confirm - Button used in footer. Must be of type zeta-button.
  * @slot {zeta-button} cancel - Button used in footer. Must be of type zeta-button.
  * @slot {zeta-button} other - Button used in footer. Must be of type zeta-button.
@@ -35,8 +33,10 @@ export const DialogIconList: DialogIcons[] = ["block", "info", "verified", "warn
  * @part footer - Styles the dialog footer
  * @part header - Styles the dialog header
  *
- * @cssproperty --dialog-max-width - Max width of the dialog. Defaults to 480px.
+ * @cssproperty --dialog-width - Width of the dialog. Defaults to 480px.
  * @cssproperty --dialog-max-height - Max height of the dialog. Defaults to 80vh.
+ * @cssproperty --dialog-title-font-size - Font size of the dialog title. Defaults to 1.25rem.
+ * @cssproperty --dialog-title-line-height - Line height of the dialog title. Defaults to 1.5rem.
  *
  * @figma https://www.figma.com/design/JesXQFLaPJLc1BdBM4sisI/%F0%9F%A6%93-ZDS---Components?node-id=229-14&node-type=canvas&m=dev
  * @storybook https://design.zebra.com/web/storybook/?path=/docs/components-dialog--docs
@@ -62,6 +62,22 @@ export class ZetaDialog extends Contourable(Popup(LitElement)) {
     this.hide(submitter.getAttribute("value") ?? this.returnValue);
   };
 
+  //Changes the icon based on the dialog flavor
+  private getHeaderIconName(): "verified" | "warning" | "error" | "block" | "info" {
+    switch (this.flavor) {
+      case "info":
+        return "info";
+      case "success":
+        return "verified";
+      case "warning":
+        return "warning";
+      case "error":
+        return "error";
+      default:
+        return "block";
+    }
+  }
+
   private _title: string = "";
   /** Title of the dialog. */
   @property({ type: String })
@@ -74,17 +90,39 @@ export class ZetaDialog extends Contourable(Popup(LitElement)) {
   }
 
   /** Whether header text should be centered. */
-  @property({ type: Boolean, reflect: true }) centered: boolean = false;
+  // @property({ type: Boolean, reflect: true }) centered: boolean = false;
 
   /** Whether the modal is initially open. */
   @property({ type: Boolean }) initialOpen: boolean = false;
 
-  //There is only one supported value for the dialog size: "large".
-  @property({ type: String, reflect: true }) size: "large";
+  /**
+   * What type of dialog box to show.
+   * This will change the icon and icon colour shown in the header.
+   *
+   * Possible values are:
+   * - `default`: Shows black block icon.
+   * - `info`: Shows purple info icon.
+   * - `success`: Shows green verified icon.
+   * - `warning`: Shows yellow warning icon.
+   * - `error`: Shows red error icon.
+   *
+   * @type {"default" | "info" | "success" | "warning" | "error"}
+   * @defaultValue 'default'
+   */
+  @property({ type: String, reflect: true }) flavor: "default" | "info" | "success" | "warning" | "error" = "default";
 
-  @property({ type: String, reflect: true }) flavor: null | "info" | "success" | "warning" | "error" = null;
-
-  @queryAssignedElements({ slot: "icon", flatten: true }) icon!: DialogIcons;
+  /**
+   * Colour of the confirm button.
+   *
+   * Possible values are:
+   * - `primary`: Blue background.
+   * - `positive`: Green background.
+   * - `negative`: Red background.
+   *
+   * @type {"primary" | "positive" | "negative"}
+   * @defaultValue 'primary'
+   */
+  @property({ type: String }) confirmButtonFlavor: "primary" | "positive" | "negative" = "primary";
 
   /** Action button 1 (Confirm). */
   @queryAssignedElements({ slot: "confirm", flatten: true }) confirmBtn!: NodeList;
@@ -100,7 +138,7 @@ export class ZetaDialog extends Contourable(Popup(LitElement)) {
     this.requestUpdate();
     let count = 0;
     if (this.confirmBtn[0] && this.confirmBtn[0] instanceof ZetaButton) {
-      this.confirmBtn[0].flavor = "primary";
+      this.confirmBtn[0].flavor = this.confirmButtonFlavor;
       this.confirmBtn[0].shape = this.rounded ? "rounded" : "sharp";
       count++;
     }
@@ -123,9 +161,9 @@ export class ZetaDialog extends Contourable(Popup(LitElement)) {
     return html`
       <dialog .returnValue=${this.returnValue} id=${this.id} ?open=${this.initialOpen}>
         <header part="header">
-          <slot name="icon"></slot>
+          <zeta-icon part="header-icon" name=${this.getHeaderIconName()}></zeta-icon>
           <h1>${this._title}</h1>
-          <zeta-icon @click=${() => this.hide("other")} @slotchange=${this.handleActionButtonChange} name="close"></zeta-icon>
+          <zeta-icon @click=${() => this.hide("close")} name="close"></zeta-icon>
         </header>
         <div part="body"><slot></slot></div>
         <footer data-element-count=${count} part="footer">
