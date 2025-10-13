59ffc227ee075bb09df05ed711dd7c447702b222
aygurs <aydin.gursoy@zebra.com>
2025-10-13 14:41:22 +0100

    Add CSS variables to allow external styling, and update documentation.

--- src/components/checkbox/checkbox.styles.js
+++ src/components/checkbox/checkbox.styles.js
@@ -2,25 +2,36 @@ import { css } from "lit";
 export default css`
   :host {
     display: inline-block;
+    --checkbox-checked-background: var(--surface-primary);
+    --checkbox-hover-background: var(--border-hover);
+    --checkbox-border-color: var(--main-subtle);
+    --checkbox-background: var(--surface-default);
+    --checkbox-icon-color: var(--main-inverse);
   }
 
   .container {
     min-width: 20px;
     min-height: 20px;
+    border: var(--border-size-medium) solid var(--checkbox-border-color) !important;
+    background-color: var(--checkbox-background) !important;
   }
 
   :host([disabled]) *[part="icon"] {
     color: var(--main-disabled);
   }
+  :host([indeterminate]) *[part="icon"],
+  :host([checked]) *[part="icon"] {
+    --icon-color: var(--checkbox-icon-color) !important;
+  }
 
   :host([indeterminate]:not([disabled])) label,
   :host([checked]:not([disabled])) label {
     .container {
-      background-color: var(--surface-primary);
+      background-color: var(--checkbox-checked-background) !important;
     }
 
     &:hover .container {
-      background-color: var(--border-hover);
+      background-color: var(--checkbox-hover-background) !important;
     }
   }
 
