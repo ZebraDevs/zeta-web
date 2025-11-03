5b764dfacb7bd91444eaf1535d2ed1da65c653a5
aygurs <aydin.gursoy@zebra.com>
2025-11-03 10:45:15 +0000

    Add custom css props to be able to change colours of stepper. Fix edit icon border colour. Update documentation.

--- src/components/stepper/stepper-item.styles.js
+++ src/components/stepper/stepper-item.styles.js
@@ -3,7 +3,15 @@ export default css`
   :host {
     --step-title-width: 230px;
     --step-edit-icon-left: 7px;
-    --step-edit-icon-border: rgb(250, 251, 252);
+    --step-edit-icon-border-color: rgb(250, 251, 252);
+    --step-edit-icon-color: var(--main-subtle);
+    --step-success-icon-color: var(--state-default-focus);
+    --step-text-default-color: var(--main-default);
+    --step-text-active-color: var(--state-default-focus);
+    --step-background-default-color: var(--surface-default);
+    --step-background-active-color: var(--surface-primary);
+    --step-background-partial-color: var(--surface-positive-subtle);
+    --step-background-success-color: var(--surface-positive);
     position: relative;
     display: flex;
     align-items: flex-start;
@@ -14,24 +22,24 @@ export default css`
   }
 
   :host([flavor="default"]) .step-number {
-    color: var(--main-default);
+    color: var(--step-text-default-color);
   }
 
   :host([flavor="active"]) .step-number {
-    background-color: var(--surface-primary);
+    background-color: var(--step-background-active-color);
     border: none;
-    color: var(--state-default-focus);
+    color: var(--step-text-active-color);
   }
 
   :host([flavor="partial"]) .step-number {
-    background-color: var(--surface-positive-subtle);
+    background-color: var(--step-background-partial-color);
     border: 3px dashed var(--main-positive);
   }
 
   :host([flavor="success"]) .step-number {
-    background-color: var(--surface-positive);
+    background-color: var(--step-background-success-color);
     border: none;
-    --icon-color: var(--state-default-focus);
+    --icon-color: var(--step-success-icon-color);
   }
   /*Get rid of counter step on success flavor*/
   :host([flavor="success"]) .step-number::before {
@@ -69,8 +77,8 @@ export default css`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: var(--surface-default);
-    color: var(--main-default);
+    background-color: var(--step-background-default-color);
+    color: var(--step-text-default-color);
     border: 1px solid var(--main-light);
     font: var(--headline-small);
     border-radius: 50%;
@@ -80,11 +88,11 @@ export default css`
   /*Edit Icon - Pen & Value Styling*/
   :host([editing]) .step-number zeta-icon[name="edit"] {
     position: relative;
-    --icon-color: var(--main-subtle);
+    --icon-color: var(--step-edit-icon-color);
     left: var(--step-edit-icon-left);
     top: var(--spacing-medium);
     --icon-border-width: 6px;
-    --icon-border-color: var(--step-success-icon-border);
+    --icon-border-color: var(--step-edit-icon-border-color);
   }
   :host([editing]) .step-number zeta-icon[name="check_mark"] + zeta-icon[name="edit"] {
     left: 3px;
