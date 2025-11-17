23f0d0050ddc4d7ea851b5f46887b953b477f51d
Aydin Gursoy <132852380+aygurs@users.noreply.github.com>
2025-11-17 13:55:33 +0000

    style(UX-1543): Control whether dropdown menu matches dropdown button width (#152)
    Exposes a prop which allows users to control whether the dropdown menu
    width matches that of the dropdown menu button.
    
    Also moves the click event listener outside of render for memory
    purposes.
    
    Fixes uneven padding on checkbox and radio dropdown items.

--- src/components/dropdown/dropdown-menu/dropdown-menu-button.styles.js
+++ src/components/dropdown/dropdown-menu/dropdown-menu-button.styles.js
@@ -11,7 +11,7 @@ export default css`
 
   .droppable-item {
     height: var(--spacing-6xl);
-    padding-left: var(--spacing-medium);
+    padding: 0 var(--spacing-medium);
   }
 
   .droppable-item:hover {
